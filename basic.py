449927fd25e45d7c186b5b7d6c564eae0dbf0aa3
ansarimohammedbilal <ansaribilal5p@gmail.com>
2025-11-04 00:41:20 +0530

    basic of python

---
+++ basic.py
@@ -0,0 +1,12 @@
+name = 'bilal'
+print(name)
+rollno = '20250801394'
+print(rollno)
+college = 'dy patil international university '
+print(college)
+a=2
+b=6
+print(a+b)
+print(a/b)
+print(a//b)
+print(a**b)
